512be30fd10955db13d74a4dd76acdd2fd2a1283
Carlos Fegurgur <carlosfegurgur@gmail.com>
2024-09-18 11:06:38 -0500

    - Update dark mode for navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import { Button } from "../button/Button";
 
 export function Navbar() {
   const [active, setActive] = useState(false);
+  const [darkMode, setDarkMode] = useState(localStorage.getItem('theme'))
 
   const handleOnClick = () => {
     document.body.classList.toggle(styles.open, !active);
@@ -17,11 +18,13 @@ export function Navbar() {
     document.body.classList.add(styles["dark-mode"]);
     document.body.classList.add(heroStyles["dark-mode"]);
     localStorage.setItem("theme", "dark");
+    setDarkMode("dark");
   };
   const disableDarkMode = () => {
     document.body.classList.remove(styles["dark-mode"]);
     document.body.classList.remove(heroStyles["dark-mode"]);
     localStorage.setItem("theme", "light");
+    setDarkMode("light");
   };
 
   const detectColorScheme = () => {
@@ -120,7 +123,7 @@ export function Navbar() {
             </ul>
           </div>
         </nav>
-        <Button isLink text="Contact" href="/contact" secondary />
+        <Button isLink text="Contact" href="/contact" secondary={darkMode === 'dark' ? true : false}/>
         {/* <!--Dark Mode toggle, uncomment button code if you want to enable a dark mode toggle--> */}
         <button
           id={styles["dark-mode-toggle"]}
@@ -134,7 +137,6 @@ export function Navbar() {
           >
             <path
               d="M459.782 347.328c-4.288-5.28-11.488-7.232-17.824-4.96-17.76 6.368-37.024 9.632-57.312 9.632-97.056 0-176-78.976-176-176 0-58.4 28.832-112.768 77.12-145.472 5.472-3.712 8.096-10.4 6.624-16.832S285.638 2.4 279.078 1.44C271.59.352 264.134 0 256.646 0c-132.352 0-240 107.648-240 240s107.648 240 240 240c84 0 160.416-42.688 204.352-114.176 3.552-5.792 3.04-13.184-1.216-18.496z"
-              fill="#f2f2f2"
             />
           </svg>
           <Image
