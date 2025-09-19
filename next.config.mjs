46c821c993004b01160f6480217f14cfae2e591c
Carlos Fegurgur <carlosfegurgur@gmail.com>
2025-09-19 14:05:36 -0500

    - update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                port: ''
-            }
-        ]
-    }
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+      },
+    ],
+  },
+  output: "export",
+  trailingSlash: true,
 };
 export default nextConfig;
