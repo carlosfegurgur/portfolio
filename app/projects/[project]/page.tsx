cc86a31cc808f5fb3434ee70b0ee85925a39ac9c
Carlos Fegurgur <carlosfegurgur@gmail.com>
2025-10-21 10:52:10 -0500

    - Comment out project pages

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,45 +1,45 @@
-import styles from "../projects.module.css";
-import { getAllProjects, getProjectData, NotionProjectData } from "@/app/utils/notionApi";
+// import styles from "../projects.module.css";
+// import { getAllProjects, getProjectData, NotionProjectData } from "@/app/utils/notionApi";
 
-export default async function Project({
-  params,
-}: {
-  params: {
-    project: string;
-  };
-}) {
-    console.log('project', params);
-      // Fetch all projects
-  const allProjects = await getAllProjects();
-  console.log('all projects', allProjects)
+// export default async function Project({
+//   params,
+// }: {
+//   params: {
+//     project: string;
+//   };
+// }) {
+//     console.log('project', params);
+//       // Fetch all projects
+//   const allProjects = await getAllProjects();
+//   console.log('all projects', allProjects)
   
-  // Find the specific project by ID
-  const projectData = allProjects.find((p:NotionProjectData) => p.properties.Slug?.rich_text[0].plain_text === params.project);
-  console.log('project data', projectData)
+//   // Find the specific project by ID
+//   const projectData = allProjects.find((p:NotionProjectData) => p.properties.Slug?.rich_text[0].plain_text === params.project);
+//   console.log('project data', projectData)
 
-  const formattedData = getProjectData(projectData);
+//   const formattedData = getProjectData(projectData);
   
-  if (!projectData) {
-    return <div>Project not found</div>;
-  }
-  return (
-    <div id={styles.project}>
-      <div className={styles.container}>I am a project page for project {formattedData.title}!</div>
-    </div>
-  );
-}
+//   if (!projectData) {
+//     return <div>Project not found</div>;
+//   }
+//   return (
+//     <div id={styles.project}>
+//       <div className={styles.container}>I am a project page for project {formattedData.title}!</div>
+//     </div>
+//   );
+// }
 
-// Generate paths for all projects at build time
-export async function generateStaticParams() {
-  const projects = await getAllProjects();
-  const projectSlugs: string[] = [];
-  projects.map((project: NotionProjectData) => {
-    if (project.properties.Slug?.rich_text?.[0]?.plain_text) {
-      projectSlugs.push(project.properties.Slug.rich_text[0].plain_text);
-    }
-  });
+// // Generate paths for all projects at build time
+// export async function generateStaticParams() {
+//   const projects = await getAllProjects();
+//   const projectSlugs: string[] = [];
+//   projects.map((project: NotionProjectData) => {
+//     if (project.properties.Slug?.rich_text?.[0]?.plain_text) {
+//       projectSlugs.push(project.properties.Slug.rich_text[0].plain_text);
+//     }
+//   });
   
-  return projectSlugs.map((slug: string) => ({
-    project: slug,
-  }));
-}
+//   return projectSlugs.map((slug: string) => ({
+//     project: slug,
+//   }));
+// }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,37 +1,37 @@
-import { Section } from "../components/layout";
-import { getAllProjects, getProjectData, NotionProjectData } from "../utils/notionApi";
-import styles from "./projects.module.css";
-import { Card as ProjectCard } from "../components/common";
-import Link from "next/link";
+// import { Section } from "../components/layout";
+// import { getAllProjects, getProjectData, NotionProjectData } from "../utils/notionApi";
+// import styles from "./projects.module.css";
+// import { Card as ProjectCard } from "../components/common";
+// import Link from "next/link";
 
-const ProjectList = async () => {
-  const projects = await getAllProjects();
-  console.log('projects', projects)
-  return (
-    <>
-      {projects.map((project: NotionProjectData, index: number) => {
-        const { slug } = getProjectData(project);
-        return (
-          <Link id={slug} href={slug} key={`${slug} - ${index}`}>
-            <ProjectCard {...getProjectData(project)} key={index} />
-          </Link>
-        );
-      })}
-    </>
-  );
-};
+// const ProjectList = async () => {
+//   const projects = await getAllProjects();
+//   console.log('projects', projects)
+//   return (
+//     <>
+//       {projects.map((project: NotionProjectData, index: number) => {
+//         const { slug } = getProjectData(project);
+//         return (
+//           <Link id={slug} href={slug} key={`${slug} - ${index}`}>
+//             <ProjectCard {...getProjectData(project)} key={index} />
+//           </Link>
+//         );
+//       })}
+//     </>
+//   );
+// };
 
-const Projects = () => {
-  return (
-    <Section id="Project Page">
-      <div className={styles.header}>
-        <h1>Project Page</h1>
-      </div>
-      <div className={styles.content}>
-        <ProjectList />
-      </div>
-    </Section>
-  );
-};
+// const Projects = () => {
+//   return (
+//     <Section id="Project Page">
+//       <div className={styles.header}>
+//         <h1>Project Page</h1>
+//       </div>
+//       <div className={styles.content}>
+//         <ProjectList />
+//       </div>
+//     </Section>
+//   );
+// };
 
-export default Projects;
+// export default Projects;
